2b7369b31d289eb81b4e5fb82b2a142d7644e59f
Juan Giraldo <JUSEGIDU@HOTMAIL.COM>
2022-09-28 22:38:42 -0500

    Autenticación configurada

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
+  get "/about", to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
