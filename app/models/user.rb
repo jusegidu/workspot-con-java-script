2f4567b0bdbbdb489664b393906f185091c53cc8
Juan Giraldo <JUSEGIDU@HOTMAIL.COM>
2022-10-06 19:13:36 -0500

    Lista la foto

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
   has_many :bookings
   has_many :places
   validates :first_name, :last_name, :phone_number, :dni, presence: true
@@ -9,4 +10,3 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
-
